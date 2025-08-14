fb0044bc77d8403274ed0419936d6e5d403803d0
Rodrigo Camargo <rodrigopimentelcamargo@yahoo.com.br>
2025-08-14 13:52:01 -0300

    java(sample): adiciona classe principal @SpringBootApplication para corrigir repackage

---
+++ standards/backend-java/exemplo-spring/src/main/java/org/ita/parque/ExemploSpringApplication.java
@@ -0,0 +1,11 @@
+package org.ita.parque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemploSpringApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ExemploSpringApplication.class, args);
+    }
+}
